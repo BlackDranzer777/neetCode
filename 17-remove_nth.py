91ce2c2edaa1559e43338b9bce86e96726335b26
BlackDranzer777 <35491850+BlackDranzer777@users.noreply.github.com>
2024-03-20 22:54:36 +0530

    Add files via upload

---
+++ 17-remove_nth.py
@@ -0,0 +1,63 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = ListNode(data)
+        if self.head is None:
+            self.head = new_node
+            new_node.next = None
+        else:
+            current = self.head
+            while current.next != None:
+                current = current.next
+            current.next = new_node
+        return
+    
+    def display(self):
+        if self.head is None:
+            print("list is empty")
+            return
+        current = self.head
+        while current is not None:
+            if current.next is not None:
+                print(current.val, end='-->')
+            else:
+                print(current.val, end='-->Null')
+            current = current.next
+        print()  # Print a newline after displaying the linked list
+        return
+    
+    def removeNth(self,position):
+        dummy = ListNode(0, self.head)
+        l = dummy
+        r = dummy.next
+        count = 0
+        while count<position and r:
+            r = r.next
+            count = count+1
+        
+        while r:
+            l = l.next
+            r = r.next
+        
+        l.next = l.next.next
+        return dummy.next
+        
+    
+    # def reOrderList(self):
+LL1 = LinkedList()
+for i in range(10):
+    LL1.append(i+1)
+print("display before redordering : ",end="")
+LL1.display()
+LL1.removeNth(3)
+print("display after redordering : ",end="")
+LL1.display()
