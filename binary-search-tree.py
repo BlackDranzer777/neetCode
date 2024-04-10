836c13bf9ed324b2b20056563441feb584124327
BlackDranzer777 <35491850+BlackDranzer777@users.noreply.github.com>
2024-04-10 22:02:09 +0200

    Add files via upload

--- binary-search-tree.py
+++ binary-search-tree.py
@@ -4,66 +4,62 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
 
-    def add_child(self, data):
-        if data == self.data:
-            return # node already exist
-
-        if data < self.data:
-            if self.left:
-                self.left.add_child(data)
-            else:
-                self.left = BinarySearchTreeNode(data)
-        else:
-            if self.right:
-                self.right.add_child(data)
-            else:
-                self.right = BinarySearchTreeNode(data)
-
-
-    def search(self, val):
+    def add_child(self, val):
         if self.data == val:
-            return True
-
+            return
+        
         if val < self.data:
             if self.left:
-                return self.left.search(val)
+                self.left.add_child(val)
             else:
-                return False
+                self.left = BinarySearchTreeNode(val)
 
-        if val > self.data:
+        else:
             if self.right:
-                return self.right.search(val)
+                self.right.add_child(val)
             else:
-                return False
-
+                self.right = BinarySearchTreeNode(val)
+    
     def in_order_traversal(self):
         elements = []
-        if self.left:
-            elements += self.left.in_order_traversal()
 
+        #visit left nodes first
+        if self.left:
+            elements+= self.left.in_order_traversal()
+
+        #visit base node
         elements.append(self.data)
 
+        #visit right node
         if self.right:
-            elements += self.right.in_order_traversal()
+            elements+= self.right.in_order_traversal()
 
         return elements
+    
+    def find_max(self):
+        marker = self
+        while marker.right:
+            marker = marker.right
+        return marker.data
+    
+    def find_min(self):
+        marker = self
+        while marker.left:
+            marker = marker.left
+        return marker.data
 
+#Helper Function to create trees
 
-def build_tree(elements):
-    print("Building tree with these elements:",elements)
-    root = BinarySearchTreeNode(elements[0])
-
-    for i in range(1,len(elements)):
-        root.add_child(elements[i])
-
+def build_tree(numbers):
+    root = BinarySearchTreeNode(numbers[0])
+    for i in range(1, len(numbers)):
+        root.add_child(numbers[i])
     return root
 
+
 if __name__ == '__main__':
-    countries = ["India","Pakistan","Germany", "USA","China","India","UK","USA"]
-    country_tree = build_tree(countries)
-
-    print("UK is in the list? ", country_tree.search("UK"))
-    print("Sweden is in the list? ", country_tree.search("Sweden"))
-
-    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
-    print("In order traversal gives this sorted list:",numbers_tree.in_order_traversal())
+    elements = [17, 23, 5, 2, 47, 28, 4, 1, 6, 56, 11, 15, -1]
+    bst = build_tree(elements)
+    print(bst.in_order_traversal())
+    print(bst.find_max())
+    print(bst.find_min())
