092aef28ed1d301fd337ab87e94904d9c022615c
BlackDranzer777 <35491850+BlackDranzer777@users.noreply.github.com>
2024-04-15 21:40:40 +0200

    Add files via upload

---
+++ 199-Binary-Tree-Right-Side-View.py
@@ -0,0 +1,22 @@
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        queue = deque()
+        nodes = []
+        if root:
+            queue.append(root)
+        level = 0
+        while len(queue) > 0:
+            for i in range(len(queue)):
+                curr = queue.popleft()
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            nodes.append(curr.val)
+        return nodes
